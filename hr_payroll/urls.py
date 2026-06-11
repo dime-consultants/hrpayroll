191f2f13966f39118f491d96674b5db8e4ac112f
charlesdete <138102397+charlesdete@users.noreply.github.com>
2026-06-11 11:00:56 +0300

    Update urls.py

--- hr_payroll/urls.py
+++ hr_payroll/urls.py
@@ -7,13 +7,14 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
     SpectacularRedocView,
 )
-
+from users.views import SignInView 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.api.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/', SignInView.as_view(), name='signin')
 ]
 
 if settings.DEBUG:
