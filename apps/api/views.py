a86063537514bc38099b973afcf8a5d95f809b9d
Stephen Osiru <144010401+sosiru@users.noreply.github.com>
2026-08-20 14:51:52 +0300

    Remove unused import for parse_payroll_upload

--- apps/api/views.py
+++ apps/api/views.py
@@ -10,7 +10,6 @@ from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from apps.api.tasks import parse_payroll_upload
 
 from apps.organizations.models import CheckoffOrganizationMirror, HRUser, AuditLog
 from apps.payroll.models import PayrollUpload, SalaryDeduction
